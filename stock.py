5ca7990fefd709ad0937ec7a1df35569ee9714d9
Aristotle Calamba <aris@cmu.edu>
2014-01-22 18:30:23 -0500

    initial file

---
+++ stock.py
@@ -0,0 +1,37 @@
+import urllib
+
+def getDailyPrice(tckr, startDate, endDate):
+    """
+    Retrieve historical prices for the given ticker, tckr.
+    The date should be formated as 'YYYY-MM-DD'
+    
+    Returns a nested list.
+    """
+    s = startDate.split('-')
+    e = endDate.split('-')
+    url = 'http://ichart.yahoo.com/table.csv?s=%s&' % tckr + \
+          'd=%s&' % str(int(e[1]) - 1) + \
+          'e=%s&' % str(int(e[2])) + \
+          'f=%s&' % str(int(e[0])) + \
+          'g=d&' + \
+          'a=%s&' % str(int(s[1]) - 1) + \
+          'b=%s&' % str(int(s[2])) + \
+          'c=%s&' % str(int(s[0])) + \
+          'ignore=.csv'
+    days = urllib.urlopen(url).readlines()
+    quote = [day[:-2].split(',') for day in days]
+    
+    return quote[1:]
+
+
+data = getDailyPrice("twtr","2013-11-07","2014-01-22")
+#print data
+print data[::-1] 
+#Sn+1 = Sn + a(EP- Sn)
+#     = (1-a)Sn + a*EP
+# initial a = 0.02
+# max a = 0.2
+# EP = max point in uptrend, min point in downtrend
+
+def parabolicSAR():
+   print "A"
